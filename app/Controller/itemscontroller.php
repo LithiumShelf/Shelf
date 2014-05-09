71e008e8c8c60e21bb35dda0bf0f52d4f2a4b6ab
changhaitravis <changhaitravis@gmail.com>
2014-05-08 21:20:28 -0700

    itemscontroller modified to conform to new view
    Moved "findbyuser" and "findbycategory" to "searchresults"

--- app/Controller/itemscontroller.php
+++ app/Controller/itemscontroller.php
@@ -30,21 +30,31 @@
             $this->set('item', $this->Item->select($id));
         }
         
-        function findbyuser($userid){
-            $params = array(':userid' => $userid);
-            $this->set('inventory', $this->Item->query('SELECT Item.* FROM Item JOIN Account ON (Item.LenderID = Account.id) WHERE Account.id = :userid', $params));
+        function findbyuser(){
+            //Get Friends
+            $params = array(':userid' => $_SESSION['userid']);
+            $this->set('friends', $this->Account->query('SELECT * FROM loztwodc_shelf.friends WHERE User = :userid;)', $params));
+            //Get Local
+            //find hot item owners in your area
+            $params = array(':userid' => $_SESSION['userid']);
+            $this->set('localusers', $this->Account->query('SELECT a2.* FROM Account a1 JOIN Account a2 ON (a1.LocationID = a2.LocationID) WHERE a1.id = :userid)', $params)); 
         }
         
         function findbycategory(){
-            if(isset($_GET["category"])){
-                $category = $_GET["category"];
-            }
-            $params = array(':category' => $category);
-            $this->set('items', $this->Item->query('SELECT Item.* FROM Item JOIN Category ON (Category.id = Item.CategoryID) WHERE Category.Category = :category', $params));
+
         }
 
         function searchresults(){
-            
+            if(isset($_GET["category"])){
+                $category = $_GET["category"];
+                $params = array(':category' => $category);
+                $this->set('items', $this->Item->query('SELECT Item.* FROM Item JOIN Category ON (Category.id = Item.CategoryID) WHERE Category.Category = :category', $params));
+            }
+            if(isset($_GET["userid"])){
+                $userid = $_GET["userid"];
+                $params = array(':userid' => $userid);
+                $this->set('items', $this->Item->query('SELECT Item.* FROM Item JOIN Account ON (Item.LenderID = Account.id) WHERE Account.id = :userid', $params));
+            }
         }
     }
 ?>
