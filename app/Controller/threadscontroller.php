b0b067e9c38d999143a9b4ecc0f08c940d69a3e0
changhaitravis <changhaitravis@gmail.com>
2014-05-13 15:28:58 -0700

    Wrote changestatus and compose message
    Untested as usual....
    Complete the viewthreads controller, then debug everything and then
    done.

--- app/Controller/threadscontroller.php
+++ app/Controller/threadscontroller.php
@@ -10,20 +10,23 @@
         }
         
         function itemrequest(){
+            $hashCode = hash('CRC32', $_POST['itemid'] . $_SESSION['userid'] );
             $params = array(':itemid' => $_POST['itemid'],
                             ':BorrowerID' => $_SESSION['userid'],
-                            ':DueDate' => $_POST['DueDate'],
-                            ':threadstatus' => "Requested"
+                            ':DueDate' => null, //$_POST['DueDate'],
+                            ':threadstatus' => "Requested",
+                            ':hashCode' => $hashCode
                             );
-            $this->set('thread', $this->Thread->query('INSERT INTO Thread (ThreadStatus, BorrowerID, ItemID, DueDate)
-                                                      VALUES (:threadstatus, :BorrowerID, :itemid, :DueDate)', $params));
+            $this->set('thread', $this->Thread->query('INSERT INTO Thread (ThreadStatus, BorrowerID, ItemID, DueDate, HashCode)
+                                                      VALUES (:threadstatus, :BorrowerID, :itemid, :DueDate, :hashCode)', $params));
         }
 
         function itemoffer(){
             $params = array(':itemid' => $_POST['itemid'],
                             ':BorrowerID' => $_POST['userid'],
-                            ':DueDate' => $_POST['DueDate'],
-                            ':threadstatus' => "Offered"
+                            ':DueDate' => null, //$_POST['DueDate'],
+                            ':threadstatus' => "Offered",
+                            ':hashCode' => $hashCode
                             );
             $this->set('thread', $this->Thread->query('INSERT INTO Thread (ThreadStatus, BorrowerID, ItemID, DueDate)
                                                       VALUES (:threadstatus, :BorrowerID, :itemid, :DueDate)', $params));
@@ -34,14 +37,20 @@
 			-
 		*/
         function changestatus(){
+            /*
+             *@Params
+             *$_SESSION[":userid"]
+             *$_POST['id'] (Thread id)
+             *
+             */
             global $page;
             $threadid = $_POST['id'];
             $params = array(':userid' => $_SESSION[":userid"],
                             ':threadid' => $threadid);
             if($page == "borrow"){
-               $thread = $this->Thread->query('SELECT Thread.id FROM Thread JOIN Item ON (Thread.ItemID = Item.id) WHERE Thread.id = :threadid AND Thread.BorrowerID = :userid', $params)[0];
+               $thread = $this->Thread->isBorrower($params);
             } elseif ($page == "lend"){
-               $thread = $this->Thread->query('SELECT Thread.id FROM Thread JOIN Item ON (Thread.ItemID = Item.id) WHERE Thread.id = :threadid AND Item.LenderID = :userid', $params)[0];
+               $thread = $this->Thread->isLender($params);
             }else{
                 die("Error");
             }
@@ -49,52 +58,72 @@
             if(isset($thread)){
 
                 switch($page){
+                    
                     case "borrow":
                         switch($thread["ThreadStatus"]){
                             case "offered": //borrower -> requested
-
+                                $changeStatusTo = "requested";
                             case "approved": //borrower -> current
                             //force borrower to enter hashcode
                             //set availability to unavailable
-
+                                if(isset($_POST['hashCode']) && $_POST['hashCode'] == $thread["hashCode"]){
+                                    $changeStatusTo = "current";
+                                    $availability = "unavailable";
+                                    $this->Thread->givePoint($thread["BorrowerID"], "borrow");
+                                    $this->Thread->givePoint($thread["LenderID"], "lend");
+                                    $this->Thread->givePoint($thread["LenderID"], "success");
+                                }else{
+                                    die("incorrect code");
+                                }
                              default: //fail and die
+                                die("One or more parameters are out of bounds, please contact Shelf development team");
                         }
+                        
                     case "lend":
                         switch($thread["ThreadStatus"]){
-                            case "requested": //lender -> approve
+                            case "requested": //lender -> approve or reject
                                 //display hashcode to lender (handled in view)
-                            
-                            case "approved": //borrower -> current
-                                //force borrower to enter hashcode
-
+                                $changeStatusTo = $_POST['action']; //"approved" or "rejected"
                             case "current": //lender -> "rejected", "complete", "late", "failed"
                                 //increment lend for lender and borrow for borrower
                                 /*
-                                    Set to either "rejected", "complete", "late", "failed"
+                                    Set to either "complete", "late", "failed"
                                 */
-
+                                $changeStatusTo = "$_POST['action']"; //complete or failed
+                                if($changeStatusTo == "complete"){
+                                    if(isset($thread["DueDate"]) && date("Y-m-d H:i:s") > $thread["DueDate"]){
+                                        $changeStatusTo = "late";
+                                    }
+                                    $this->Thread->givePoint($thread["BorrowerID"], "borrow");
+                                    $availability = "available";
+                                }else{
+                                    $changeStatusTo = "failed";
+                                    $availability = "missing";
+                                }
                             default: //fail and die
+                                die("One or more parameters are out of bounds, please contact Shelf development team");
                         }
-
                     default://fail and die
+                    die("One or more parameters are out of bounds, please contact Shelf development team");
                 }
-
                 
-                $currentstatus = $this->Thread->query('', $params)
                 //change thread status
-                $params = array(':itemid' => $itemid,
-                                ':nextstatus' => $nextstatus,
-                                ':availability' => $_POST('availability'));
                 $this->set('thread', $this->Thread->query('UPDATE Thread SET ThreadStatus=:newstatus WHERE id=:threadid'));
                 if($availability){
-                    $this->set('itemlock', $this->Thread->query('UPDATE Item SET ItemStatus=:availability WHERE (SELECT ItemID FROM Thread WHERE id=:threadid)', $params));
+                    $params = array(':itemid' => $thread["ItemID"],
+                        ':nextstatus' => $changeStatusTo,
+                        ':availability' => $availability);
+                    $this->set('itemlock', $this->Thread->query('UPDATE Item SET ItemStatus=:availability WHERE id = :itemid', $params));
                 }
+                //change item availability
+            }else{
+                echo "There was an error";
             }
 
         }
         
         function viewallthreads(){
-			global $page;
+		global $page;
             $params = array(':UserID' => 2 /*$_SESSION['userid']*/);
             if($page != "feed"){
                 //Select all your threads
@@ -128,14 +157,37 @@
             $params = array(':id' => $id);
             $this->set('thread', $this->Thread->query('SELECT * FROM Thread JOIN Message ON (Message.ThreadID = Thread.id) WHERE id = :id', $params));
             // ADD CODE TO UPDATE THE ABOVE TABLE AND MARK ALL hasRead as "true"
+            
+            // If fromBorrower = 1, print Borrower Name, If 0, print your User's name
         }
         
-        function composemessage($threadID){
+        function composemessage(){
+            global $page;
             //add a new message to a thread
+            $params = array(':userid' => $_SESSION[":userid"],
+                            ':threadid' => $threadid);
+            if($page == "borrow"){
+               $thread = $this->Thread->isBorrower($params);
+
+            } elseif ($page == "lend"){
+               $thread = $this->Thread->isLender($params);
+            }else{
+                die("Error");
+            }
+            if(isset($thread)){
+                switch($page){
+                    case "borrow":
+                        $isBorrower = 1;
+                    case "lend":
+                        $isBorrower = 0;
+                    default:
+                        die("Parameter out of bounds");
+            }
             $params = array(':subject' => $_POST['subject'],
                             ':body' => $_POST['body'],
-                            ':thread' => $threadID);
-            $this->set('message', $this->Thread->query('INSERT INTO Message VALUES (NULL, NOW(), :subject, :body, false, :thread)', $params));
+                            ':thread' => $_POST['threadID'],
+                            ':isBorrower' => $isBorrower);
+            $this->set('message', $this->Thread->query('INSERT INTO Message VALUES (NULL, NOW(), :subject, :body, 0, :thread, :isBorrower)', $params));
         }
         
         function readmessage(){
--- app/Model/thread.php
+++ app/Model/thread.php
@@ -23,5 +23,29 @@
         function joinitem($params, $queryAppend = null) {
             
         }
+        
+        function isBorrower($params){
+            //$params = array(':userid' => $_SESSION[":userid"],
+            //                ':threadid' => $threadid);
+            return $this->Thread->query('SELECT Thread.id, Thread.ItemID, Thread.ThreadStatus, Thread.BorrowerID, Item.LenderID, Thread.DueDate, Thread.HashCode FROM Thread JOIN Item ON (Thread.ItemID = Item.id) WHERE Thread.id = :threadid AND Thread.BorrowerID = :userid', $params)[0];
+        }
+        
+        function isLender($params){
+            //$params = array(':userid' => $_SESSION[":userid"],
+            //                ':threadid' => $threadid);
+            return $this->Thread->query('SELECT Thread.id, Thread.ItemID, Thread.ThreadStatus, Thread.BorrowerID, Item.LenderID, Thread.DueDate, Thread.HashCode FROM Thread JOIN Item ON (Thread.ItemID = Item.id) WHERE Thread.id = :threadid AND Item.LenderID = :userid', $params)[0];
+        }
+        
+        function givePoint($id, $role){ //$role is lend/borrow/success
+            $params = array(':id' => $id);
+            $switch($role){
+                case "lend":
+                    return $this->Thread->query('UPDATE Account SET Lent = Lent + 1 WHERE id = :id', $params);
+                case "borrow":
+                    return $this->Thread->query('UPDATE Account SET Borrowed = Borrowed + 1 WHERE id = :id', $params);
+                case "success":
+                    return $this->Thread->query('UPDATE Account SET numSuccessful = numSuccessful + 1 WHERE id = :id', $params);
+            }
+        }
     }
 ?>
