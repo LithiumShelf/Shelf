ab45d8da1ecfd53500c605329ba8c4e58e901ddd
justnuggets <jnguyen0749@gmail.com>
2014-05-15 11:22:11 -0700

    View All Threads update
    -Responsive buttons, same depth
    -Output if no threads
    -Text links to corresponding profile and item

--- app/View/threads/viewallthreads.php
+++ app/View/threads/viewallthreads.php
@@ -6,7 +6,6 @@ switch ($type) {
 			$itemname = $thread['Name'];
 			$src = $thread['ItemPic'];
 			$person = $thread['bFirst']." ".$thread['bLast'];
-			$asin = $thread['ASIN'];
 		 ?>
 		<li>
 			<!--Feed Tile: Top-->
@@ -16,10 +15,18 @@ switch ($type) {
 				</a>
 			</div>
 			
-			<p><strong><?=$person?></strong> is borrowing <strong><?=$itemname ?></strong>.</p>
+			<p>
+				<a href="more/accounts/profile/<?= $thread['BorrowerID']?>">
+					<strong><?=$person?></strong> 
+				</a>
+				is borrowing 
+				<a href="feed/items/itempage/<?=$thread['ItemID']?>" >
+					<strong><?=$itemname ?></strong>.
+				</a>
+			</p>
 			
 			<!--Feed Tile: Image-->
-			<a href="borrow/items/searchresults?search_item=<?=$itemname?>">
+			<a href="feed/items/itempage/<?= $thread['ItemID'] ?>">
 					<img src="<?= $BASE_URL ?>/images/item/<?=$src?> ?>" alt="<?=$itemname?>">
 			</a>
 			
@@ -35,134 +42,165 @@ switch ($type) {
 		//LOOP: Counter for when to separate Currently Requested from Borrowing, (fencepost)
 		$newsection = 0; 
 		$currentstat = "Open";
-		foreach ($threads as $thread) { 
-			$status = $thread['ThreadStatus'];
-			$due = $thread['DueDate'];
-			$done = $status =="Closed" || $status == "Rejected" || $status =="Late" || $status == "Failed";
-		//HEADER: If transition from Currently Requested to Borrowing, increment counter
-			if($newsection == 0 && $status=="Current" ){  
-				$newsection++; 
-				?>	
-				</ul>
-				<h1>Borrowing</h1>
-				<ul>
-			<?php } else if ($newsection <= 1 && $done) { 
-				$newsection= 2;
-				$currentstat="Closed";
-				?>
-				</ul>
-				<div data-role="collapsible">
-					<h1>Borrow History</h1>
+		if ($threads) {
+			foreach ($threads as $thread) { 
+				$status = $thread['ThreadStatus'];
+				$due = $thread['DueDate'];
+				$done = $status =="Closed" || $status == "Rejected" || $status =="Late" || $status == "Failed";
+			//HEADER: If transition from Currently Requested to Borrowing, increment counter
+				// To make it so that they still show up even if empty, remove else if...
+				if($newsection == 0 && $status=="Current" ){  
+					$newsection++; 
+					?>	
+					</ul>
+					<h1>Borrowing</h1>
 					<ul>
-			<?php 
-				}
-			?>
-			
-			
-			<li class="<?= $currentstat?>">
-				<div style="float: left;">
-					<a href="more/accounts/profile/<?= $thread['LenderID']?>">
-						<img src="<?= $thread['profilePic'] ?>" style="float: left; width: 50px; height: 50px;">
-					</a>
-				</div>
 				
-			<!--PLACEHOLDER: Link to thread on click-->
-				<a href="http://www.google.com#q=thread">
-					<div >
-						<!--General-->
-						<strong><?= $thread['Name'] ?></strong> from <strong><?= $thread['firstName']." ".$thread['lastName'] ?></strong> <br> 
-						<!--Reputation-->
-						Borrowed <?= $thread['Borrowed'] ?> Lent <?= $thread['Lent'] ?> <br>
-						<!--Status-->
-						<?= $status ?>
-						<?= $due ?>
+				<?php } else if ($newsection <= 1 && $done) { 
+					$newsection= 2;
+					$currentstat="Closed";
+					?>
+					</ul>
+					<div data-role="collapsible" data-collapsed="false">
+						<h1>Borrow History</h1>
+						<ul>
+				<?php 
+					}
+				?>
+				
+				
+				<li class="<?= $currentstat?>" id="<?=$thread['id']?>">
+					<div style="float: left;">
+						<a href="more/accounts/profile/<?= $thread['LenderID']?>">
+							<img src="<?= $thread['profilePic'] ?>" style="float: left; width: 50px; height: 50px;">
+						</a>
 					</div>
-				</a>
-				
-			<!--BUTTONS-->
-				<div style = "clear:left;">
-					<?php
-						if($status=="Open"|| $status=="Current" ) {
-					?>
-					<button type="button" name="current">Return</item> 
-					<?php  } else if ($status=="Requested" ) { 
-					?>
+					
+				<!--PLACEHOLDER: Link to thread on click-->
+	
+						<div >
+							<!--General-->
+							<a href="borrow/items/itempage/<?= $thread['ItemID']?>">
+								<strong><?= $thread['Name'] ?></strong>  
+							</a>
+							from
+							<a href="more/accounts/profile/<?= $thread['LenderID']?>">
+								<strong><?= $thread['firstName']." ".$thread['lastName'] ?></strong> 
+							</a>
+							<br> 
+							<!--Reputation-->
+							Borrowed <?= $thread['Borrowed'].' ' ?> Lent <?= $thread['Lent'] ?> <br>
+							<!--Status-->
+							<?= $status ?>
 
-					<button type="button" name="requested" value="complete">Receive</button>
-					<button type="button" name="requested" value="failed">Cancel</button>
-					<?php } else {
-					} ?>
-				</div>
-			</li>
-		<?php }
-		if ($newsection > 1) {
-		?>
-		</div>
-		<?php }
+						</div>
+				
+					
+				<!--BUTTONS-->
+					<div style = "clear:left;" class="ui-grid-b ui-responsive">
+						<?php
+							if($status=="Open"|| $status=="Current" ) {
+						?>
+						<div><button type="button" name="current">Return</button> </div>
+						<?php  } else if ($status=="Requested" ) { 
+						?>
+
+						<div class="ui-block-a"> 
+							<button type="button" name="requested" value="complete">Receive</button>
+						</div>
+						<div class="ui-block-b">
+							<button type="button" name="requested" value="failed">Cancel</button>
+						</div>
+						<?php } else {
+						} ?>
+					</div>
+				</li>
+			<?php }
+			if ($newsection > 1) {
+			?>
+			</div>
+			<?php } 
+		}else { 
+			print "No recent activity";
+		}
 		break;
 		
     case "lend":
 		//Counter to determine new section header (fencepost)
 		$newsection = 0; 
-		foreach ($threads as $thread) { 
-			$status = $thread['ThreadStatus'];
-			$due = $thread['DueDate'];
-			$done = $status == "Complete" || $status =="Closed" || $status == "Rejected" || $status =="Late" || $status == "Failed";
-			
-			//HEADER: From 'To Review' to  'Currently Lending'
-			if ($newsection == 0 && $status=="Current") {
-				$newsection++;?>
-				</ul> <br>
-				<h1>Current Lending</h1><br>
-				<ul>
-			<?php
-			//HEADER: From 'Currently Lending' to 'Past Lending'
-			} else if ($newsection <= 1 && $done) {
-				$newsection++;?>
-				</ul> <br>
-				<div data-role="collapsible">
-					<h1>Past Lending</h1><br>
-					<ul>
-			<?php }
-			?>
-			<li class="<?= $status ?>">
-				<div style="float:left">
-					<a href="more/accounts/profile/<?= $thread['BorrowerID']?>">
-						<img src="<?= $thread['profilePic'] ?>" style="float: left; width: 50px; height: 50px;">
-					</a>
-				</div>
+		if ($threads) {
+			foreach ($threads as $thread) { 
+				$status = $thread['ThreadStatus'];
+				$due = $thread['DueDate'];
+				$done = $status == "Complete" || $status =="Closed" || $status == "Rejected" || $status =="Late" || $status == "Failed";
 				
-			<!--PLACEHOLDER: Link to thread on click-->
-				<a href="http://www.google.com#q=thread">
+				//HEADER: From 'To Review' to  'Currently Lending'
+				if ($newsection == 0 && $status=="Current") {
+					$newsection++;?>
+					</ul> 
+					<h1>Current Lending</h1>
+					<ul>
+				<?php
+				//HEADER: From 'Currently Lending' to 'Past Lending'
+				} else if ($newsection <= 1 && $done) {
+					$newsection++;?>
+					</ul> 
+					<div data-role="collapsible">
+						<h1>Past Lending</h1>
+						<ul>
+				<?php }
+				?>
+				<li class="<?= $status ?>" id="<?=$thread['id']?>">
+					<div style="float:left">
+						<a href="more/accounts/profile/<?= $thread['BorrowerID']?>">
+							<img src="<?= $thread['profilePic'] ?>" style="float: left; width: 50px; height: 50px;">
+						</a>
+					</div>
+					
+				<!--PLACEHOLDER: Link to thread on click-->
 					<div>
 						<!--General-->
-						<strong><?= $thread['firstName']." ".$thread['lastName'] ?></strong> - <strong><?= $thread['Name'] ?></strong>  <br> 
+						<a href="more/accounts/profile/<?= $thread['BorrowerID']?>">
+							<strong><?= $thread['firstName']." ".$thread['lastName'] ?></strong> 
+						</a>
+						- 
+						<a href="lend/items/itempage/<?= $thread['ItemID']?>">
+							<strong><?= $thread['Name'] ?></strong>  
+						</a>
+						<br> 
 						<!--Reputation-->
-						Borrowed <?= $thread['Borrowed'] ?> Lent <?= $thread['Lent'] ?> <br>
+						Borrowed <?= $thread['Borrowed'].' ' ?> Lent <?= $thread['Lent'] ?> <br>
 						<?= $status ?>
-						<?= $due ?>
+
 						<!--Latest Message-->
 					</div>
-				</a>	
-				
-			<!--BUTTONS-->
-				<div style = "clear:left;">
-					<?php
-						if($status=="Open"|| $status=="Current" ) {
-					?>
-					<button type="button" name="current">Take Back</item> 
-					<?php  } else if ($status=="Requested" ) { 
-					?>
-
-					<button type="button" name="requested" value="approved">Approve</button>
-					<button type="button" name="requested" value="rejected">Reject</button>
-					<?php } else {
-					} ?>
+					
+					
+				<!--BUTTONS-->
+					<div style = "clear:left;" class="ui-grid-a ui-responsive">
+						<?php
+							if($status=="Open"|| $status=="Current" ) {
+						?>
+						<div><button type="button" name="current">Take Back</button> </div>
+						<?php  } else if ($status=="Requested" ) { 
+						?>
+						<div class="ui-block-a" style = "clear:none;">
+							<button type="button" name="requested" value="approved">Approve</button>
+						</div>
+						
+						<div class="ui-block-b" style = "clear:none;">
+							<button type="button" name="requested" value="rejected">Reject</button>
+						</div>
+						<?php } else {
+						} ?>
+					</div>
+				</li>
+			<?php }
+			if ($newsection > 1) {	?>
 				</div>
-			</li>
-		<?php }
-		if ($newsection > 1) {	?>
-			</div>
-		<?php }
+			<?php }
+		} else { 
+			print "No recent activity";
+		}
         break;
 }?>
