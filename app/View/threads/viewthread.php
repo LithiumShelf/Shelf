b7ee2ce4ac4412078ae10d77663a9bb4ea3a2183
Travis Juntara <changhaitravis@gmail.com>
2014-05-15 01:21:34 -0700

    viewThreads/fetchMessage writ

--- app/View/threads/viewthread.php
+++ app/View/threads/viewthread.php
@@ -1,3 +1,25 @@
 <?php
     //In view, If fromBorrower = 1, print Borrower Name, If 0, print your User's name
+    
+    foreach($messages as $message){
+        if(fromBorrower == 1){
+            $from = $message["BorrowerName"];
+            $to = $message["LenderName"];
+        }else{
+            $from = $message["LenderName"];
+            $to = $message["BorrowerName"];
+        }
+?>
+        <div class="message">
+            <p><b>From: <?= $from ?></b> <br>
+                <b>To: <?= $to ?></b></p>
+            <h4 class="messagesubject"><?= $message["MessageSubject"] ?></h4>
+            <p class="messagebody">$message["Body"]</p>
+            <?php if($message["hasRead"] == 1){ ?>
+            <span class="ui-btn ui-icon-eye ui-btn-icon-left">hasRead</a>
+            <?php } ?>
+            <p><i>Sent on <?= $message["Messagetimestamp"] ?></i></p>
+        </div>
+<?php        
+    }
 ?>
