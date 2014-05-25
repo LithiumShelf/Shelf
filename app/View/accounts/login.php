1ef565483649ad1851231f08c4a0e4d8e2d4f1ee
changhaitravis <changhaitravis@gmail.com>
2014-05-24 18:35:33 -0700

    Added link to login page, now centered in chrome

--- app/View/accounts/login.php
+++ app/View/accounts/login.php
@@ -6,10 +6,19 @@
      if($user){
         //session_start();
         $_SESSION["userid"] = $user[0]["id"];
-        echo "You are now logged in";
+        echo "You are now logged in ";
+        echo '<a href="/" data-ajax="false">Click here </a>';
+        echo "to go home, if not you'll be redirected in 10 seconds";
      }else{   echo "Wrong Username or Password";
      }
     
     //then start the session and redirect to /feed/
     
 ?>
+
+<script>
+var redirTime = "10000";
+var redirURL = "http://www.dontbeshelfish.com";
+$( document ).ready(redirTimer);
+function redirTimer() { self.setTimeout("self.location.href = redirURL;",redirTime); }
+</script>
